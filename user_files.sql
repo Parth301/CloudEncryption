677a6c1ac164a1d0e9993e07f97decaea8ae00d5
Parth301 <127748701+Parth301@users.noreply.github.com>
2024-12-14 21:34:03 +0530

    user_files.sql

---
+++ user_files.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `user_files` (
+  `id` int(11) PRIMARY KEY,
+  `user_email` varchar(255) NOT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `file_data` longblob NOT NULL,
+  `encryption_key` varchar(255) NOT NULL,
+  `iv` varchar(255) NOT NULL,
+  `upload_date` timestamp NOT NULL DEFAULT current_timestamp()
+);
